1a0b0189dd923dfb90f66965c9db5eeee2eae8b1
dcontt00 <dcontt00@estudiantes.unileon.es>
2025-03-13 11:26:13 +0100

    Created GET and PUT routes for Portfolio

--- src/routes/portfolio.ts
+++ src/routes/portfolio.ts
@@ -44,4 +44,75 @@ router.post("/", authenticate, async (req, res) => {
     }
 });
 
+router.get("/", authenticate, async (req, res) => {
+    try {
+        const user = req.user;
+
+        if (!user) {
+            res.status(404).json({
+                status: 404,
+                success: false,
+                message: "User not found",
+            });
+            return;
+        }
+
+        const portfolios = await portfolioModel.find({user: user.id});
+
+        res.status(200).json({
+            status: 200,
+            success: true,
+            data: portfolios,
+        });
+    } catch (err: any) {
+        res.status(400).json({
+            status: 400,
+            success: false,
+            message: err.message,
+        });
+    }
+});
+
+router.put("/:url", authenticate, async (req, res) => {
+    try {
+        const user = req.user;
+
+        if (!user) {
+            res.status(404).json({
+                status: 404,
+                success: false,
+                message: "User not found",
+            });
+            return;
+        }
+
+        const portfolio = await portfolioModel.findOneAndUpdate(
+            {url: req.params.url, user: user.id},
+            req.body,
+            {new: true}
+        );
+
+        if (!portfolio) {
+            res.status(404).json({
+                status: 404,
+                success: false,
+                message: "Portfolio not found",
+            });
+            return;
+        }
+
+        res.status(200).json({
+            status: 200,
+            success: true,
+            data: portfolio,
+        });
+    } catch (err: any) {
+        res.status(400).json({
+            status: 400,
+            success: false,
+            message: err.message,
+        });
+    }
+})
+
 export default router;
