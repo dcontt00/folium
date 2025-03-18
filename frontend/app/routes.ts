1d19e64924a7e888e82f4b8f7bc3b0d9693ee576
dcontt00 <dcontt00@estudiantes.unileon.es>
2025-03-18 11:10:34 +0100

    Removed data route

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -2,7 +2,6 @@ import {index, route, type RouteConfig} from "@react-router/dev/routes";
 
 export default [
     index("routes/home.tsx"),
-    route("/data", "routes/data.tsx"),
     route("/login", "routes/login.tsx"),
     route("/register", "routes/register.tsx"),
 
