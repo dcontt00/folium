8e78a22b7086eb3c6093951b7127f482d634e870
dcontt00 <dcontt00@estudiantes.unileon.es>
2025-03-18 12:20:55 +0100

    Show portfolios on home

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,7 +1,9 @@
 import type {Route} from "./+types/home";
-import {Welcome} from "../welcome/welcome";
-import {AppShell, Burger, Group, Skeleton} from "@mantine/core";
+import {Welcome} from "~/welcome/welcome";
+import {AppShell, Burger, Flex, Group, Skeleton} from "@mantine/core";
 import {useDisclosure} from "@mantine/hooks";
+import axios from "axios";
+import PortfolioCard from "~/components/PortfolioCard";
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -10,9 +12,27 @@ export function meta({}: Route.MetaArgs) {
     ];
 }
 
-export default function Home() {
-    const [opened, {toggle}] = useDisclosure();
+interface Portfolio {
+    title: string;
+    description: string;
+    url: string;
+}
 
+export async function clientLoader({params}: Route.ClientLoaderArgs) {
+    const res = await axios.get(`http://localhost:3000/portfolio`, {withCredentials: true});
+    const portfolios: Array<Portfolio> = res.data.data;
+    return portfolios;
+}
+
+
+// HydrateFallback is rendered while the client loader is running
+export function HydrateFallback() {
+    return <div>Loading...</div>;
+}
+
+export default function Home({loaderData}: Route.ComponentProps) {
+    const [opened, {toggle}] = useDisclosure();
+    const portfolios: Array<Portfolio> = loaderData;
     return (
         <AppShell
             header={{height: 60}}
@@ -26,13 +46,31 @@ export default function Home() {
             </AppShell.Header>
             <AppShell.Navbar p="md">
                 Navbar
-                {Array(15)
-                    .fill(0)
-                    .map((_, index) => (
-                        <Skeleton key={index} h={28} mt="sm" animate={false}/>
-                    ))}
+                {portfolios.length > 0 ? (
+                    portfolios.map((portfolio: Portfolio, index) => (
+                        <div key={index}>
+                            <h1>{portfolio.title}</h1>
+                            <p>{portfolio.description}</p>
+                            <a href={portfolio.url}>Visit</a>
+                        </div>
+                    ))
+
+                ) : (
+                    Array(15)
+                        .fill(0)
+                        .map((_, index) => <Skeleton key={index} h={28} mt="sm" animate={false}/>)
+                )}
+
             </AppShell.Navbar>
             <AppShell.Main>
+                <Flex>
+                    {
+                        portfolios.map((portfolio: Portfolio, index) => (
+                            <PortfolioCard key={index} title={portfolio.title} description={portfolio.description}
+                                           url={portfolio.url}/>
+                        ))
+                    }
+                </Flex>
                 <Welcome/>
             </AppShell.Main>
         </AppShell>
