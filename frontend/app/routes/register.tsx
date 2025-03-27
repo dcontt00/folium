b642b38de6893c30ea3b82981945ee1267b28e90
dcontt00 <dcontt00@estudiantes.unileon.es>
2025-03-27 12:41:41 +0100

    Added validation to register

--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -1,6 +1,6 @@
 import type {Route} from "./+types/home";
-import {AppShell, Button, PasswordInput, Stack, TextInput, Title} from "@mantine/core";
-import {useForm} from '@mantine/form';
+import {AppShell, Button, Container, PasswordInput, Stack, TextInput, Title} from "@mantine/core";
+import {isEmail, isNotEmpty, useForm} from '@mantine/form';
 import axios from "axios";
 import {useNavigate} from "react-router";
 
@@ -20,6 +20,26 @@ interface FormValues {
     password: string;
 }
 
+function validatePassword(value: string): string | null {
+    let errorMessage = 'Password must have: ';
+
+    switch (true) {
+        case value.length < 8:
+            errorMessage += '8 characters, ';
+        case !/[A-Z]/.test(value):
+            errorMessage += '1 uppercase letter, ';
+        case !/\d/.test(value):
+            errorMessage += '1 number, ';
+        case !/[!@#$%^&*]/.test(value):
+            errorMessage += '1 special symbol. ';
+    }
+
+    if (errorMessage === 'Password must have: ') {
+        return null;
+    }
+
+    return errorMessage
+}
 
 export default function Register() {
     const navigate = useNavigate();
@@ -32,10 +52,16 @@ export default function Register() {
             email: '',
             password: '',
         },
+        validateInputOnChange: true,
 
         validate: {
-            email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+            email: isEmail('Invalid email'),
+            password: validatePassword,
+            name: isNotEmpty('Name required'),
+            surname: isNotEmpty('Surname required'),
+            username: isNotEmpty('Username required'),
         },
+
     });
 
 
@@ -55,49 +81,52 @@ export default function Register() {
                 padding="xl"
             >
                 <AppShell.Main>
-
-                    <Stack
-                        align="stretch"
-                        justify="center"
-                        gap="md"
-                    >
-                        <Title order={3}>Register</Title>
-                        <TextInput
-                            withAsterisk
-                            label="Name"
-                            key={form.key('name')}
-                            {...form.getInputProps('name')}
-                        />
-
-                        <TextInput
-                            withAsterisk
-                            label="Surname"
-                            key={form.key('surname')}
-                            {...form.getInputProps('surname')}
-                        />
-                        <TextInput
-                            withAsterisk
-                            label="Username"
-                            key={form.key('username')}
-                            {...form.getInputProps('username')}
-                        />
-
-                        <TextInput
-                            withAsterisk
-                            label="Email"
-                            key={form.key('email')}
-                            {...form.getInputProps('email')}
-                        />
-                        <PasswordInput
-                            withAsterisk
-                            label="Password"
-                            key={form.key('password')}
-                            {...form.getInputProps('password')}
-                        />
+                    <Container size="xs">
 
 
-                        <Button type="submit">Submit</Button>
-                    </Stack>
+                        <Stack
+                            align="stretch"
+                            justify="center"
+                            gap="md"
+                        >
+                            <Title order={3}>Register</Title>
+                            <TextInput
+                                withAsterisk
+                                label="Name"
+                                key={form.key('name')}
+                                {...form.getInputProps('name')}
+                            />
+
+                            <TextInput
+                                withAsterisk
+                                label="Surname"
+                                key={form.key('surname')}
+                                {...form.getInputProps('surname')}
+                            />
+                            <TextInput
+                                withAsterisk
+                                label="Username"
+                                key={form.key('username')}
+                                {...form.getInputProps('username')}
+                            />
+
+                            <TextInput
+                                withAsterisk
+                                label="Email"
+                                key={form.key('email')}
+                                {...form.getInputProps('email')}
+                            />
+                            <PasswordInput
+                                withAsterisk
+                                label="Password"
+                                key={form.key('password')}
+                                {...form.getInputProps('password')}
+                            />
+
+
+                            <Button type="submit" disabled={!form.isValid()}>Submit</Button>
+                        </Stack>
+                    </Container>
                 </AppShell.Main>
             </AppShell>
         </form>
