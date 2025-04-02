1c7cc5f058cc47c04ebe661e1911dd7d0ebb3101
dcontt00 <dcontt00@estudiantes.unileon.es>
2025-04-02 12:56:11 +0200

    Implementing DnD in ContainerComponent.tsx

--- frontend/app/components/edit/EditComponentSection.tsx
+++ frontend/app/components/edit/EditComponentSection.tsx
@@ -1,7 +1,14 @@
-import type {ButtonComponentType, ComponentType, ImageComponentType, TextComponentType} from "~/interfaces/interfaces";
+import type {
+    ButtonComponentType,
+    ComponentType,
+    ContainerComponentType,
+    ImageComponentType,
+    TextComponentType
+} from "~/interfaces/interfaces";
 import EditTextComponent from "~/components/edit/editComponents/EditTextComponent";
 import EditButtonComponent from "~/components/edit/editComponents/EditButtonComponent";
 import EditImageComponent from "~/components/edit/editComponents/EditImageComponent";
+import EditContainerComponent from "~/components/edit/editComponents/EditContainerComponent";
 
 
 interface Props {
@@ -24,6 +31,10 @@ export default function EditComponentSection({component, onEditComponent}: Props
             case "ImageComponent":
                 const imageComponent = component as ImageComponentType;
                 return <EditImageComponent component={imageComponent} onEditComponent={onEditComponent}/>
+            case "ContainerComponent":
+                const containerComponent = component as ContainerComponentType;
+                return <EditContainerComponent containerComponent={containerComponent}
+                                               onEditComponent={onEditComponent}/>
             default:
                 return <div>Component not found</div>
         }
--- frontend/app/components/portfolioComponents/ContainerComponent.tsx
+++ frontend/app/components/portfolioComponents/ContainerComponent.tsx
@@ -1,6 +1,9 @@
-import type {ContainerComponentType} from "~/interfaces/interfaces"
+import type {ComponentType, ContainerComponentType} from "~/interfaces/interfaces"
 import Component from "~/components/portfolioComponents/Component";
-
+import {DragDropContext, Draggable, Droppable, type DropResult} from "@hello-pangea/dnd";
+import {IconEdit, IconMenu2, IconTrash} from "@tabler/icons-react";
+import {ActionIcon} from "@mantine/core";
+import {useState} from "react";
 
 interface Props {
     containerComponent: ContainerComponentType
@@ -8,12 +11,58 @@ interface Props {
 
 export default function ContainerComponent({containerComponent}: Props) {
 
+    const [containerState, setContainerState] = useState(containerComponent);
+
+    const handleDragEnd = (result: DropResult) => {
+        if (!result.destination) {
+            return;
+        }
+
+        const newComponents = Array.from(containerComponent.components);
+        const [movedComponent] = newComponents.splice(result.source.index, 1);
+        newComponents.splice(result.destination.index, 0, movedComponent);
+
+        for (let i = 0; i < newComponents.length; i++) {
+            newComponents[i].index = i;
+        }
+
+        const newContainer = {...containerState, components: newComponents};
+        setContainerState(newContainer);
+    };
 
     return (
         <div style={{display: 'flex', flexDirection: 'row', gap: 20, alignItems: 'center'}}>
-            {containerComponent.components.map((component, index) => (
-                <Component key={index} component={component}/>
-            ))}
+            <DragDropContext onDragEnd={handleDragEnd}>
+                <Droppable droppableId="containerComponents" direction="horizontal">
+                    {(provided) => (
+                        <div {...provided.droppableProps} ref={provided.innerRef}
+                             style={{display: 'flex', flexDirection: 'row'}}>
+                            {containerState.components.map((component: ComponentType, index: number) => (
+                                <Draggable key={component._id} draggableId={component._id} index={index}>
+                                    {(provided) => (
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}
+                                            className="containerComponent"
+                                        >
+                                            <IconMenu2 className="icon"/>
+                                            <Component component={component}/>
+                                            <ActionIcon className="icon">
+                                                <IconEdit/>
+                                            </ActionIcon>
+                                            <ActionIcon color="red" className="icon">
+                                                <IconTrash/>
+                                            </ActionIcon>
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
         </div>
     )
 }
