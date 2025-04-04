4292d1aa7c008cefd9b56426e7b2e850144d3f6e
dcontt00 <dcontt00@estudiantes.unileon.es>
2025-04-04 13:13:52 +0200

    Added loader

--- frontend/app/routes/portfolio.tsx
+++ frontend/app/routes/portfolio.tsx
@@ -4,8 +4,11 @@ import type {Route} from "~/routes";
 import type {Portfolio} from "~/interfaces/interfaces";
 import axiosInstance from "~/axiosInstance";
 import type {AxiosResponse} from "axios";
+import {Center, Loader} from "@mantine/core";
 
 export async function clientLoader({params}: Route.ClientLoaderArgs) {
+
+    // Wait 5 seconds
     const portfolio: Portfolio = await axiosInstance.get(`/portfolio/${params.url}`)
         .then((response: AxiosResponse) => {
             return response.data.data;
@@ -14,6 +17,15 @@ export async function clientLoader({params}: Route.ClientLoaderArgs) {
             throw data(responseError.data.message, {status: responseError.status});
         });
     return portfolio;
+}
+
+// HydrateFallback is rendered while the client loader is running
+export function HydrateFallback() {
+    return (
+        <Center style={{height: '100vh', width: '100vw'}} bg="var(--mantine-color-gray-light)">
+            <Loader color="blue" size="xl" type="dots"/>
+        </Center>
+    );
 }
 
 export default function Portfolio({loaderData}: Route.ComponentProps) {
