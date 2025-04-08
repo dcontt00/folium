e715aec6ae01672b2143a8faaf2b27976c5a71d6
dcontt00 <dcontt00@estudiantes.unileon.es>
2025-04-08 10:48:52 +0200

    Changes to component

--- frontend/app/components/edit/History.tsx
+++ frontend/app/components/edit/History.tsx
@@ -1,7 +1,8 @@
-import {Button, Group, Stack, Text, Timeline, Title} from "@mantine/core";
+import {Accordion, Button, Group, List, Stack, Text, ThemeIcon, Timeline, Title} from "@mantine/core";
 import {useEffect, useState} from "react";
 import config from "~/config";
 import axiosInstance from "~/axiosInstance";
+import {IconEdit, IconPlus, IconTrash} from "@tabler/icons-react";
 
 interface Version {
     _id: string;
@@ -26,6 +27,7 @@ export default function History({portfolioId}: Props) {
         async function fetchHistory() {
             try {
                 const response = await axiosInstance.get(`${config.BACKEND_URL}/portfolio/${portfolioId}/versions`);
+                console.log(response.data.data);
                 setData(response.data.data);
             } catch (err: any) {
                 setError(err);
@@ -48,6 +50,43 @@ export default function History({portfolioId}: Props) {
                     <Timeline.Item title="Change">
                         <Text c="dimmed" size="sm">{version.title}</Text>
                         <Text size="xs" mt={4}>{version.createdAt}</Text>
+                        <Accordion>
+                            <Accordion.Item value="Changes">
+                                <Accordion.Control>Changes</Accordion.Control>
+                                <Accordion.Panel>
+                                    <List>
+                                        <List.Item
+                                            icon={
+                                                <ThemeIcon color="red" size={24} radius="xl">
+                                                    <IconTrash size={16}/>
+                                                </ThemeIcon>
+                                            }
+                                        >
+                                            Removed TextComponent
+                                        </List.Item>
+                                        <List.Item
+                                            icon={
+                                                <ThemeIcon color="green" size={24} radius="xl">
+                                                    <IconPlus size={16}/>
+                                                </ThemeIcon>
+                                            }
+                                        >
+                                            Added Image
+                                        </List.Item>
+                                        <List.Item
+                                            icon={
+                                                <ThemeIcon color="blue" size={24} radius="xl">
+                                                    <IconEdit size={16}/>
+                                                </ThemeIcon>
+                                            }
+                                        >
+                                            Changed title
+                                        </List.Item>
+                                    </List>
+                                </Accordion.Panel>
+                            </Accordion.Item>
+
+                        </Accordion>
                         <Group>
                             <Button size="compact-md">Restore to this</Button>
                             <Button size="compact-md">Preview</Button>
