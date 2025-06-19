a1fda5f2dbf1e02bdb4af68ef5f4906c115c0b15
dcontt00 <dcontt00@estudiantes.unileon.es>
2025-06-19 11:15:51 +0200

    Fixes

--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -8,36 +8,36 @@ import {IServiceResult} from "@/interfaces";
 const saltRounds = 10
 
 async function createUser(name: string, surname: string, username: string, email: string, password: string): Promise<IServiceResult> {
-    try {
-        const user = await UserModel.create({
+    return await UserModel
+        .create({
             name: name,
             surname: surname,
             username: username,
             email: email,
             password: await bcrypt.hash(password, saltRounds),
-        });
+        })
+        .then(result => {
+            return {
+                status: 201,
+                success: true,
+                data: {
+                    name: result.name,
+                    email: result.email,
+                    username: result.username,
+                    _id: result._id
+                },
+            };
+        })
+        .catch(err => {
+            if (err.code === 11000) {
+                const duplicatedKey = Object.keys(err.keyValue)[0];
+                throw new ApiError(400, "A user with this " + duplicatedKey + " already exists");
+            } else {
+                throw new ApiError(400, "Error creating user");
+            }
+        })
 
-        const userResponse = {
-            name: user.name,
-            email: user.email,
-            username: user.username,
-            _id: user._id,
-        };
 
-        return {
-            status: 201,
-            success: true,
-            data: userResponse,
-        };
-
-    } catch (err: any) {
-        if (err.code === 11000) {
-            const duplicatedKey = Object.keys(err.keyValue)[0];
-            throw new ApiError(400, "A user with this " + duplicatedKey + " already exists");
-        } else {
-            throw new ApiError(400, "Error creating user");
-        }
-    }
 }
 
 async function login(email: string, password: string) {
